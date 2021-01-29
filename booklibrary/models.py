97b7c5ab0a9fa5f435737e417107eb2656e4d18d
Carroll <ACarroll@smartronix.com>
2021-01-28 20:40:33 -0500

    Copied models over from previous project

--- booklibrary/models.py
+++ booklibrary/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+class Genre(models.Model):
+    category = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.category)
+
+class Series(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+        
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    notes = models.CharField(max_length=5000, default="No notes", blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT)
+    series = models.ForeignKey(Series, on_delete=models.PROTECT, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.title)

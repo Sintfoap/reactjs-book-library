b23871723333ac7dbe59b3b950883d0d4225ed97
Sintfoap <github7@ohbrc.com>
2021-07-26 17:01:11 -0400

    fixed the acsessing author inside of bookpage issues

--- booklibrary-react/src/components/Detail_formatter.js
+++ booklibrary-react/src/components/Detail_formatter.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function BuildDetailFormatter(prefix, id_attribute="id") {
     return (value, row) => {
         if(row[id_attribute]){
-            return <Link style={{color: 'black'}} to={prefix + row[id_attribute].toString()}>{value}</Link>
+            return <Link style={{color: 'black'}} to={(prefix + row[id_attribute].id).toString()}>{value}</Link>
         }
         return null
     }
